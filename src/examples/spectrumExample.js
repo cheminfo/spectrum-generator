550e4fd3551220c4db8127f13a6e32775263329d
Océane Patiny <oceane@patiny.com>
2018-07-12 11:45:13 +0200

    spectrumExample.js works

--- src/examples/spectrumExample.js
+++ src/examples/spectrumExample.js
@@ -1,12 +1,19 @@
 // generate a spectrum with spectrum-generator
-var generateSpectrum = require('../index.js');
+import SG from '..';
 
+const debug = require('debug')('example');
 const fs = require('fs');
+
+const addBaseline = require('../functionalities/addBaseline');
+const addNoise = require('../functionalities/addNoise');
+
+var generateSpectrum = SG.generateSpectrum;
 
 var options = { start: 0, end: 40, pointsPerUnit: 1 };
 
 const peaks = [[4, 10], [20, 30], [236, 1], [569, 76]];
 const spectrum = generateSpectrum(peaks, options);
 
+debug(spectrum);
 
 fs.writeFileSync(`${__dirname}/data.json`, JSON.stringify(spectrum), 'utf8');
