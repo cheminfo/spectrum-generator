d3dd12f688842c63a246fdc9e0b43a9f3dd9ecad
Luc Patiny <luc@patiny.com>
2018-11-16 06:53:32 +0100

    add GC generation example

---
+++ examples/generateGC.js
@@ -0,0 +1,34 @@
+/*
+This example with generate and save a spectrum as a 'data.json'
+using directly the method generateSpectrum
+
+Because it use ES6 module you need to execute this code
+with transpilation on the fly using
+node -r esm generateSpectrum.js
+*/
+
+import { generateSpectrum } from '../src';
+
+const fs = require('fs');
+
+const xMax = 25;
+const peakWidth = 0.1;
+
+
+const options = { start: 0, end: xMax, pointsPerUnit: 100, noise: { percent: 2 }, peakWidthFct: (x) => peakWidth };
+
+let peaks = [];
+for (let i = 0; i < 20; i++) {
+  peaks.push([Math.random() * xMax, Math.random() * 100]);
+}
+const spectrum = generateSpectrum(peaks, options);
+
+let text = [];
+for (let i = 0; i < spectrum.x.length; i++) {
+  text.push(`${String(spectrum.x[i].toFixed(2)).padStart(7)} ${String(spectrum.y[i].toFixed(4)).padStart(10)}`);
+}
+
+fs.writeFileSync(`${__dirname}/data.txt`, text.join('\n'), 'utf8');
+
+
+fs.writeFileSync(`${__dirname}/data.json`, JSON.stringify(spectrum), 'utf8');
