368f3a96de7a5903c3d1de5043666e8e8e9ec7ca
Michaël Zasso <targos@protonmail.com>
2020-02-24 09:24:58 +0100

    chore: fix build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ export default {
   input: 'src/index.js',
   output: {
     format: 'cjs',
-    file: 'lib/index.js'
-  }
+    file: 'lib/index.js',
+  },
+  external: ['d3-random', 'ml-xsadd'],
 };
