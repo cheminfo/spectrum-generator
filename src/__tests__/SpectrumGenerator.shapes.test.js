81c37f18a49c07304e15cdb0b85b5548df51c0e6
Luc Patiny <luc@patiny.com>
2020-10-15 11:52:25 +0200

    test: add second test for SpectrumGenerator

--- src/__tests__/SpectrumGenerator.shapes.test.js
+++ src/__tests__/SpectrumGenerator.shapes.test.js
@@ -1,3 +1,4 @@
+import { xyMaxYPoint } from 'ml-spectra-processing';
 import { SpectrumGenerator } from '..';
 
 describe('SpectrumGenerator various shapes', () => {
@@ -38,5 +39,52 @@ describe('SpectrumGenerator various shapes', () => {
     expect(ys[70]).toBe(10);
 
     expect(ys[31] !== ys[71]).toBe(true);
+  });
+
+  it('second test', () => {
+    let spectrumGenerator = new SpectrumGenerator({
+      from: 0,
+      to: 10,
+      nbPoints: 101,
+      peakWidthFct: () => 0.1,
+      shape: {
+        kind: 'lorentzian',
+        options: {
+          length: 13,
+          fwhm: 4,
+        },
+      },
+    });
+    spectrumGenerator.addPeak(
+      { x: 2.5, y: 2 },
+      {
+        width: 0.1,
+        shape: {
+          kind: 'lorentzian',
+          options: {
+            length: 13,
+            fwhm: 4,
+          },
+        },
+      },
+    );
+    spectrumGenerator.addPeak(
+      { x: 5, y: 1 },
+      {
+        width: 0.2,
+        shape: {
+          kind: 'gaussian',
+          options: {
+            length: 13,
+            fwhm: 4,
+          },
+        },
+      },
+    );
+    const spectrum = spectrumGenerator.getSpectrum();
+    let max = xyMaxYPoint(spectrum);
+    expect(spectrum.y[49]).toBe(0.5);
+    expect(max.x).toBe(2.5);
+    expect(max.y).toBe(2);
   });
 });
