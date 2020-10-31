47325c6bd2402432b2817c1533c76568068bac4d
Luc Patiny <luc@patiny.com>
2020-10-31 16:27:51 +0100

    chore: add generateLorentzian example

---
+++ examples/generateLorentzian.js
@@ -0,0 +1,33 @@
+/*
+This example with generate and save a spectrum as a 'data.json'
+using directly the method generateSpectrum
+
+Because it use ES6 module you need to execute this code
+with transpilation on the fly using
+node -r esm generateSpectrum.js
+*/
+
+import { generateSpectrum } from '../src';
+
+const fs = require('fs');
+
+const options = {
+  from: 0,
+  to: 100,
+  nbPoints: 101,
+  shape: {
+    kind: 'lorentzian',
+    options: {
+      fwhm: 1000,
+      length: 10001,
+    },
+  },
+};
+const peaks = [{ x: 50, y: 100, width: 10 }];
+const spectrum = generateSpectrum(peaks, options);
+
+fs.writeFileSync(
+  `${__dirname}/data.json`,
+  JSON.stringify({ x: Array.from(spectrum.x), y: Array.from(spectrum.y) }),
+  'utf8',
+);
