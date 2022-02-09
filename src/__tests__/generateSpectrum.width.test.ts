126aa95f9b8887b08a1ad6cdd7c903f2aaec7bb4
Luc Patiny <luc@patiny.com>
2022-02-09 17:19:49 +0100

    test: add failing test

---
+++ src/__tests__/generateSpectrum.width.test.ts
@@ -0,0 +1,27 @@
+import { Gaussian } from 'ml-peak-shape-generator';
+
+import { generateSpectrum } from '../SpectrumGenerator';
+
+describe('generateSpectrum width / fwhm', () => {
+  it('derivative should be continuous', () => {
+    const spectrumFWHM = generateSpectrum([{ x: 0, y: 1, fwhm: 0.2 }], {
+      generator: {
+        from: -1,
+        to: 1,
+        nbPoints: 51,
+      },
+    });
+
+    const spectrumWidth = generateSpectrum([{ x: 0, y: 1, width: 0.2 }], {
+      generator: {
+        from: -1,
+        to: 1,
+        nbPoints: 51,
+      },
+    });
+
+    expect(Array.from(spectrumWidth.y)).not.toStrictEqual(
+      Array.from(spectrumFWHM.y),
+    );
+  });
+});
