1035c7e1be527d68241fbf05a71fd076c92a0cb6
Luc Patiny <luc@patiny.com>
2022-04-29 11:35:17 +0200

    test: add test to check factor and default functions

---
+++ src/__tests__/generateSpectrum.factor.test.ts
@@ -0,0 +1,68 @@
+import { Gaussian } from 'ml-peak-shape-generator';
+import { xSum } from 'ml-spectra-processing';
+
+import { generateSpectrum } from '../SpectrumGenerator';
+import { Peak1D } from '../types/Peaks1D';
+
+describe('generateSpectrum factor', () => {
+  it('lorentzian specified', () => {
+    let peaks: Peak1D[] = [
+      {
+        x: 0,
+        y: 1,
+        shape: { kind: 'lorentzian', fwhm: 0.5 },
+      },
+    ];
+
+    const spectrum = generateSpectrum(peaks, {
+      generator: {
+        from: -0.1,
+        to: 0.1,
+        nbPoints: 51,
+        shape: { kind: 'gaussian' },
+      },
+      peakOptions: { factor: 0.01 },
+    });
+    expect(xSum(spectrum.y)).toBeCloseTo(2.9994881310384542, 6);
+  });
+
+  it('lorentzian by default', () => {
+    let peaks: Peak1D[] = [
+      {
+        x: 0,
+        y: 1,
+      },
+    ];
+
+    const spectrum = generateSpectrum(peaks, {
+      generator: {
+        from: -0.1,
+        to: 0.1,
+        nbPoints: 51,
+        shape: { kind: 'lorentzian', fwhm: 0.5 },
+      },
+      peakOptions: { factor: 0.01 },
+    });
+    expect(xSum(spectrum.y)).toBeCloseTo(2.9994881310384542, 6);
+  });
+
+  it('gaussian by default', () => {
+    let peaks: Peak1D[] = [
+      {
+        x: 0,
+        y: 1,
+      },
+    ];
+
+    const spectrum = generateSpectrum(peaks, {
+      generator: {
+        from: -0.1,
+        to: 0.1,
+        nbPoints: 51,
+        shape: { kind: 'gaussian', fwhm: 0.5 },
+      },
+      peakOptions: { factor: 0.01 },
+    });
+    expect(xSum(spectrum.y)).toBeCloseTo(2.99964514012866, 6);
+  });
+});
