70f81b0df4f7cbea6392375572d35cf48ad68509
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2025-02-26 21:28:08 -0500

    feat: support to negative peaks in 2D spectra generation(#85)
    * fix: add negative peaks with intensity bigger than 1e-6
    
    * chore: add test case
    
    * chore: fix eslint

--- src/Spectrum2DGenerator.ts
+++ src/Spectrum2DGenerator.ts
@@ -1,3 +1,4 @@
+import type { NumberArray } from 'cheminfo-types';
 import { getShape2D } from 'ml-peak-shape-generator';
 import type {
   Shape2D,
@@ -5,7 +6,7 @@ import type {
   XYNumber,
   Shape2DClass,
 } from 'ml-peak-shape-generator';
-import { matrixMinMaxZ } from 'ml-spectra-processing';
+import { matrixCreateEmpty, matrixMinMaxZ } from 'ml-spectra-processing';
 
 import type { Data2D } from './types/Data2D';
 import type { Peak2D, Peak2DSeries } from './types/Peaks2D';
@@ -28,12 +29,12 @@ const convertWidthToFWHM = (shape: Shape2DClass, width: number | XYNumber) => {
 
 export interface OptionsSG2D {
   /**
-   * First x value (inclusive).
+   * First xy value (inclusive).
    * @default `0`
    */
   from?: number | XYNumber;
   /**
-   * Last x value (inclusive).
+   * Last xy value (inclusive).
    * @default `100`
    */
   to?: number | XYNumber;
@@ -100,7 +101,7 @@ export interface Spectrum2D {
   maxY: number;
   minZ: number;
   maxZ: number;
-  z: Float64Array[] | number[][];
+  z: NumberArray[];
 }
 
 export class Spectrum2DGenerator {
@@ -146,7 +147,10 @@ export class Spectrum2DGenerator {
     this.data = {
       x: new Float64Array(nbPoints.x),
       y: new Float64Array(nbPoints.y),
-      z: createMatrix(this.nbPoints),
+      z: matrixCreateEmpty({
+        nbRows: this.nbPoints.y,
+        nbColumns: this.nbPoints.x,
+      }),
     };
 
     for (const axis of axis2D) {
@@ -292,7 +296,7 @@ export class Spectrum2DGenerator {
             this.data.x[xIndex] - position.x,
             this.data.y[yIndex] - position.y,
           );
-        if (value > 1e-6) {
+        if (Math.abs(value) > 1e-6) {
           this.data.z[yIndex][xIndex] += value;
         }
       }
@@ -369,12 +373,4 @@ function assertNumber(value: number, name: string) {
   if (!Number.isFinite(value)) {
     throw new TypeError(`${name} option must be a number`);
   }
-}
-
-function createMatrix(nbPoints: XYNumber) {
-  const zMatrix = new Array(nbPoints.y);
-  for (let i = 0; i < nbPoints.y; i++) {
-    zMatrix[i] = new Float64Array(nbPoints.x);
-  }
-  return zMatrix;
 }
